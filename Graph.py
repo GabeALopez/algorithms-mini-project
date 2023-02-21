994ed2f14cc991b341a854df7da49ed53852bd29
Ago0495 <116462335+Ago0495@users.noreply.github.com>
2023-02-21 15:41:59 -0500

    added comments
    thats it

--- Graph.py
+++ Graph.py
@@ -6,13 +6,15 @@ import random
 
 fig, graph = plt.subplots()
 
+#plots points of Nodes
 for i in range(100):
     graph.scatter(phoneArr[i].xpos, phoneArr[i].ypos, 100, c="black", alpha=0.5, marker=r'$\cdot$',label="Phone")
     
-
+#test circles
 graph.add_artist(plt.Circle((phoneArr[10].xpos, phoneArr[10].ypos), phoneArr[20].range, color = "red", fill = False))
 graph.add_artist(plt.Circle((phoneArr[20].xpos, phoneArr[20].ypos), phoneArr[20].range, color = "blue", fill = False))
 
+#size of graph
 plt.xlim([0,1000])
 plt.ylim([0,1000])
 plt.show()
--- MiniProject1.py
+++ MiniProject1.py
@@ -8,14 +8,15 @@ class Node:
 
 phoneArr = [Node] * 100
 
+#makes array of Nodes with random positions
 for i in range(100):
     randx = random.randint(0, 1000)
     randy = random.randint(0, 1000)
     phoneArr[i] = Node(randx, randy)
+    #test print
     print("Node", i, "x:", phoneArr[i].xpos, "y:", phoneArr[i].ypos)
 
-
-
+#test print
 print("Node 1", "x:", phoneArr[1].xpos, "y:", phoneArr[1].ypos)
 
 # arr = [ [0]*1000 for i in range(1000)] 
