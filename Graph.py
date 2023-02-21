6def53c91ba68e165bc5c3c30c0c3f4eb3f16681
Ago0495 <116462335+Ago0495@users.noreply.github.com>
2023-02-21 15:39:33 -0500

    array of Nodes now works
    fixed bug where positions of Nodes are overwritten

--- Graph.py
+++ Graph.py
@@ -1,19 +1,17 @@
-from MiniProject1 import Phone
+from MiniProject1 import Node
+from MiniProject1 import phoneArr
 import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-arry = [Phone] * 100
-
 fig, graph = plt.subplots()
 
 for i in range(100):
-    arry[i].xpos = random.randint(0, 1000)
-    arry[i].ypos = random.randint(0, 1000)
-    print(arry[i].xpos)
-    print(arry[i].ypos)
-    graph.scatter(arry[i].xpos, arry[i].ypos, 100, c="black", alpha=0.5, marker=r'$\cdot$',label="Phone")
+    graph.scatter(phoneArr[i].xpos, phoneArr[i].ypos, 100, c="black", alpha=0.5, marker=r'$\cdot$',label="Phone")
     
+
+graph.add_artist(plt.Circle((phoneArr[10].xpos, phoneArr[10].ypos), phoneArr[20].range, color = "red", fill = False))
+graph.add_artist(plt.Circle((phoneArr[20].xpos, phoneArr[20].ypos), phoneArr[20].range, color = "blue", fill = False))
 
 plt.xlim([0,1000])
 plt.ylim([0,1000])
--- MiniProject1.py
+++ MiniProject1.py
@@ -1,18 +1,28 @@
-class Phone:
-    radius = 5
-    def __init__(self,xpos,ypos):
+import random
+class Node:
+    def __init__(self, xpos, ypos):
         self.xpos = xpos
         self.ypos = ypos
+        self.range = 50
+        self.price = 0 
 
-phoneArr = [Phone] * 100
+phoneArr = [Node] * 100
+
+for i in range(100):
+    randx = random.randint(0, 1000)
+    randy = random.randint(0, 1000)
+    phoneArr[i] = Node(randx, randy)
+    print("Node", i, "x:", phoneArr[i].xpos, "y:", phoneArr[i].ypos)
 
 
 
-arr = [ [0]*1000 for i in range(1000)] 
+print("Node 1", "x:", phoneArr[1].xpos, "y:", phoneArr[1].ypos)
 
-for i in range(len(arr)):
-    for j in range(len(arr[i])):
-        arr[i][j] = Phone()
+# arr = [ [0]*1000 for i in range(1000)] 
+
+# for i in range(len(arr)):
+#     for j in range(len(arr[i])):
+#         arr[i][j] = Phone()
 
         
 
