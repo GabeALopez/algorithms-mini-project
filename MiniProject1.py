1ce1cef048e06302040b7f6015a5fdef66886b18
Gabriel Lopez <gabriel.andres.lopez03@gmail.com>
2023-02-21 12:11:22 -0500

    Added arrays for graph and phone objects

--- MiniProject1.py
+++ MiniProject1.py
@@ -3,4 +3,18 @@ class Phone:
     def __init__(self,xpos,ypos):
         self.xpos = xpos
         self.ypos = ypos
+
+phoneArr = [Phone] * 100
+
+
+
+arr = [ [0]*1000 for i in range(1000)] 
+
+for i in range(len(arr)):
+    for j in range(len(arr[i])):
+        arr[i][j] = Phone()
+
+        
+
+
         
