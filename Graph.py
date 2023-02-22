5bed9c4c418c59655ae53dd8fa5380a1d863f942
EthanDiemer <119145667+EthanDiemer@users.noreply.github.com>
2023-02-22 13:21:34 -0500

    added more to the greedy algorithm (it now loops, all nodes have a boolean value that enables/disables them)

--- Graph.py
+++ Graph.py
@@ -1,6 +1,6 @@
-from MiniProject1 import Node
-from MiniProject1 import phoneArr
-from MiniProject1 import k
+from createNodes import Node
+from createNodes import phoneArr
+from createNodes import k
 import matplotlib.pyplot as plt
 import numpy as np
 import random
@@ -15,14 +15,31 @@ for i in range(k):
 graph.add_artist(plt.Circle((phoneArr[10].xpos, phoneArr[10].ypos), phoneArr[20].range, color = "red", fill = False))
 graph.add_artist(plt.Circle((phoneArr[20].xpos, phoneArr[20].ypos), phoneArr[20].range, color = "blue", fill = False))
 
-#testing to find the cheapest node
-cheapest = 0;
-for i in range(100):
-    if(phoneArr[i].cost < phoneArr[cheapest].cost):
-        cheapest = i;
-graph.add_artist(plt.Circle((phoneArr[cheapest].xpos, phoneArr[cheapest].ypos), phoneArr[cheapest].range, color = "green", fill = False))
-#Then take all nodes in range of this one and disable them and disable self
-#Then run program again to find next cheapest and repeat
+#testing greedy
+#temp budget
+budget = 5
+while budget > 0:
+    cheapest = 0 #temp
+    #loop through and find the cheapest node that is enabled
+    for i in range(100):
+        if(phoneArr[i].cost < phoneArr[cheapest].cost) & (phoneArr[i].enabled):
+            cheapest = i;
+    
+    phoneArr[cheapest].enabled = False
+    
+    if (budget - phoneArr[cheapest].cost < 0):
+        break
+    else:
+        budget = budget - phoneArr[cheapest].cost
+        #test print node
+        print("Node cheap", cheapest,": ", "x:", phoneArr[cheapest].xpos, "y:", phoneArr[cheapest].ypos," cost: ",phoneArr[cheapest].cost)
+    
+    #test print budget
+    print("Budget: ",budget)
+
+    #make circle
+    graph.add_artist(plt.Circle((phoneArr[cheapest].xpos, phoneArr[cheapest].ypos), phoneArr[cheapest].range, color = "green", fill = False))
+    #Then take all nodes in range of this one and disable them
 
 
 #size of graph
