5f6713f0e0494f3c077320fba3c4ad0d8fea8cf3
EthanDiemer <119145667+EthanDiemer@users.noreply.github.com>
2023-02-23 14:01:10 -0500

    Added the Completely Random Algorithm and the Pure Greedy Algorithm

--- Graph.py
+++ Graph.py
@@ -56,6 +56,45 @@ while budget > 0:
  
     #Then take all nodes in range of this one and disable them
 
+
+# Pure Greedy Algorithm
+#
+# Might need some work
+"""
+budget = 10
+while budget > 0:
+    cheapest = 0 #temp
+    #loop through and find the cheapest node that is enabled
+    for i in range(100):
+        if(phoneArr[i].cost < phoneArr[cheapest].cost) & (phoneArr[i].visited):
+            cheapest = i;
+    
+    phoneArr[cheapest].visited = False
+    
+    if (budget - phoneArr[cheapest].cost < 0):
+        break
+    else:
+        budget = budget - phoneArr[cheapest].cost
+    #make circle
+    graph.add_artist(plt.Circle((phoneArr[cheapest].xpos, phoneArr[cheapest].ypos), phoneArr[cheapest].range, color = "black", fill = False))
+"""
+#
+#
+
+# Completly Random Algorithm
+#
+"""
+budget = 10
+while budget > 0:
+    rn = random.randint(0,99)
+    if (phoneArr[rn].visited) & (phoneArr[rn].cost < budget):
+        graph.add_artist(plt.Circle((phoneArr[cheapest].xpos, phoneArr[cheapest].ypos), phoneArr[cheapest].range, color = "pink", fill = False))
+        budget - budget - phoneArr[rn].cost
+"""
+# 
+#  
+
+
 #size of graph
 plt.xlim([0,1000])
 plt.ylim([0,1000])
