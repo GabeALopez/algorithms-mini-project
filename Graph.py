1ec395956ca72d1b0c80e43639a96dee60f99964
Ago0495 <116462335+Ago0495@users.noreply.github.com>
2023-02-23 13:01:34 -0500

    start of new changes
    trying to implement the algorithm from class

--- Graph.py
+++ Graph.py
@@ -1,4 +1,4 @@
-from createNodes import Node
+from createNodes import Phone
 from createNodes import phoneArr
 from createNodes import neighborhood
 from createNodes import k
@@ -9,7 +9,7 @@ import random
 
 fig, graph = plt.subplots()
 
-#plots points of Nodes
+#plots points of Phones
 for i in range(k):
     graph.scatter(phoneArr[i].xpos, phoneArr[i].ypos, 100, c="black", alpha=0.5, marker=r'$\cdot$',label="Phone")
     
@@ -29,15 +29,16 @@ while budget > 0:
             cheapest = i
             break
     print("Budget: ",round(budget, 2))
-    #loop through and find the cheapest node that is enabled
+    #loop through and find the cheapest Phone that is enabled
     for i in range(num):
-        if(phoneArr[i].costPerNode < phoneArr[cheapest].costPerNode) & (phoneArr[i].visited == False):
+        if(phoneArr[i].costPerPhone < phoneArr[cheapest].costPerPhone) & (phoneArr[i].visited == False):
             cheapest = i
     if (budget - phoneArr[cheapest].cost < 0):
         break
     else:
         budget = budget - phoneArr[cheapest].cost
         phoneArr[cheapest].visited = True
+<<<<<<< Updated upstream
         for i in range(num):
             if phoneArr[cheapest].inRangeArr[i] == True:
                 phoneArr[i].visited = True
@@ -48,13 +49,25 @@ while budget > 0:
                             phoneArr[i].costPerNode = round(phoneArr[i].cost / phoneArr[i].numInRange, 2)
                         else:
                             phoneArr[i].costPerNode = phoneArr[i].cost
+=======
+        for j in range(num):
+            if phoneArr[cheapest].inRangeArr[j] == True:
+                phoneArr[j].visited = True
+                for m in range(num):
+                    if phoneArr[cheapest].inRangeArr[m] == True:
+                        phoneArr[m].numInRange = phoneArr[m].numInRange - 1
+                        if phoneArr[m].numInRange > 0:
+                            phoneArr[m].costPerPhone = round(phoneArr[m].cost / phoneArr[m].numInRange + 1, 2)
+                        else:
+                            phoneArr[m].costPerPhone = phoneArr[m].cost
+>>>>>>> Stashed changes
 
-        print("Buy Node:", cheapest, "cost:", phoneArr[cheapest].cost, "costpernode:", phoneArr[cheapest].costPerNode)
+        print("Buy Phone:", cheapest, "cost:", phoneArr[cheapest].cost, "costperPhone:", phoneArr[cheapest].costPerPhone)
 
     #make circle
         graph.add_artist(plt.Circle((phoneArr[cheapest].xpos, phoneArr[cheapest].ypos), phoneArr[cheapest].range, color = "green", fill = False))
  
-    #Then take all nodes in range of this one and disable them
+    #Then take all Phones in range of this one and disable them
 
 #size of graph
 plt.xlim([0,1000])
--- createNodes.py
+++ createNodes.py
@@ -1,60 +1,61 @@
 import random
-import math
-class Node:
-    def __init__(self,num, xpos, ypos, cost):
-        self.xpos = xpos
-        self.ypos = ypos
-        self.cost = cost
-        self.costPerNode = 0
-        self.range = 50
-        self.visited = bool(0)
-        self.inRangeArr = [bool] * num
-        for i in range(num):
-            self.inRangeArr[i] = bool(0)
-        self.numInRange = 0
+import math 
         
-class Neighborhood:
+class Coordinates:
     def __init__(self, xpos, ypos):
         self.xpos = xpos
         self.ypos = ypos
+class Phone(Coordinates):
+    def __init__(self,num, xpos, ypos, cost):
+        Coordinates.__init__(self, xpos, ypos)
+        self.cost = cost
+        self.costPerPhone = 0
+        self.range = 50
+        self.visited = bool(0)
+        self.inRangeSet = set()
+        self.numInRange = 0       
+    def findCPF(self):
+        self.costPerPhone = round(self.cost/(self.numInRange + 1), 2)
         
 num = 100
 
-phoneArr = [Node] * num
-neighborhood = [Neighborhood] * 4
+phoneArr = [Phone] * num
+neighborhood = [Coordinates] * 4
 
-#makes array of Nodes with random positions
+#makes cluster of Phones in Neighborhoods with different costs
 k = 0
 for i in range(4):
     print("Neighborhood",i)
     randx = random.randint(200, 800)
     randy = random.randint(200, 800)
-    neighborhood[i] = Neighborhood(randx, randy)
+    neighborhood[i] = Coordinates(randx, randy)
     if k < 0.70 * num:
         for j in range(int(random.uniform(0.2 * num + 0.5, 0.3 * num + 0.5))):
             posX = neighborhood[i].xpos + random.randint(-100,100)
             posY = neighborhood[i].ypos + random.randint(-100,100)
             randCost = round(random.uniform(0.1 + i + i * 0.5, 1 + i * 1.5), 2)
-            phoneArr[k] = Node(num, posX, posY, randCost)
+            phoneArr[k] = Phone(num, posX, posY, randCost)
             print("Phone:", k, "cost:",phoneArr[k].cost)
-            k = k + 1
+            k += 1
     else:
         randx = random.randint(200, 800)
         randy = random.randint(200, 800)
-        neighborhood[i] = Neighborhood(randx, randy)
+        neighborhood[i] = Coordinates(randx, randy)
         for j in range(num - k):
             posX = neighborhood[i].xpos + random.randint(-100,100)
             posY = neighborhood[i].ypos + random.randint(-100,100)
             randCost = round(random.uniform(0.1 + i + i * 0.5, 1 + i * 1.5),2)
-            phoneArr[k] = Node(num, posX, posY, randCost)
+            phoneArr[k] = Phone(num, posX, posY, randCost)
             print("Phone:", k, "cost:",phoneArr[k].cost)
-            k = k + 1
-        
+            k += 1
+
+#makes set for each Phone of Phones that are within range
 for i in range(num):
     for j in range(num):
         if (i != j):
             distance = math.sqrt(math.pow(phoneArr[i].xpos - phoneArr[j].xpos, 2) + math.pow(phoneArr[i].ypos - phoneArr[j].ypos, 2))
             if (distance <= phoneArr[i].range):
+<<<<<<< Updated upstream
                 phoneArr[i].inRangeArr[j] = bool(1)
                 phoneArr[i].numInRange = phoneArr[i].numInRange + 1
             if phoneArr[i].numInRange > 0:
@@ -62,22 +63,36 @@ for i in range(num):
             else:
                 phoneArr[i].costPerNode = phoneArr[i].cost
             
+=======
+                phoneArr[i].inRangeSet.add(phoneArr[j])
+                phoneArr[i].numInRange = len(phoneArr[i].inRangeSet)
+                
+>>>>>>> Stashed changes
 for i in range(num):
-    print("Node ", i, ":", end=" ")
-    for j in range(num):
-        print(int(phoneArr[i].inRangeArr[j]), end="")
-    print("\n")
+    phoneArr[i].findCPF()
+
+for i in range(num):
+    print("Phone:", i, "numInRAnge:", phoneArr[i].numInRange, "cost:", phoneArr[i].cost, "costPerPhone:", phoneArr[i].costPerPhone)
+
+purchaseSet = set()
+budget = 10
+tempSet = set()
+for i in range(num):
+    tempSet.add(phoneArr[i])
+
+cheapest = list(tempSet)[i]
+#while len(tempSet) > 0:
+#    cheapest = list(tempSet)[0]
+for i in range(len(tempSet)):
+    if list(tempSet)[i].costPerPhone < cheapest.costPerPhone:
+        cheapest = list(tempSet)[i]
+    
+        
+print(cheapest.costPerPhone)
+
 
 #test print
-print("Node",num - 1,  "x:", phoneArr[num - 1].xpos, "y:", phoneArr[num - 1].ypos, "numInRange:", phoneArr[num - 1].numInRange, "cost:", phoneArr[num - 1].cost, "costpernode:", phoneArr[num - 1].costPerNode)
-
-# arr = [ [0]*1000 for i in range(1000)] 
-
-# for i in range(len(arr)):
-#     for j in range(len(arr[i])):
-#         arr[i][j] = Phone()
-
-        
+#print("Phone",num - 1,  "x:", phoneArr[num - 1].xpos, "y:", phoneArr[num - 1].ypos, "numInRange:", phoneArr[num - 1].numInRange, "cost:", phoneArr[num - 1].cost, "costperPhone:", phoneArr[num - 1].costPerPhone)
 
 
         
