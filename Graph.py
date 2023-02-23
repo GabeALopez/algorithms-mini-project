ecd93d5de24d0373a8c50b497930145ac41e736a
Ago0495 <116462335+Ago0495@users.noreply.github.com>
2023-02-23 15:22:08 -0500

    Working new Algorithm
    using the Algorithm the prof showed in class

--- Graph.py
+++ Graph.py
@@ -1,17 +1,18 @@
 from createNodes import Phone
-from createNodes import phoneArr
+from createNodes import phoneSet
 from createNodes import neighborhood
 from createNodes import k
 from createNodes import num
+from createNodes import purchaseSet
 import matplotlib.pyplot as plt
 import numpy as np
 import random
 
 fig, graph = plt.subplots()
 
-#plots points of Phones
-for i in range(k):
-    graph.scatter(phoneArr[i].xpos, phoneArr[i].ypos, 100, c="black", alpha=0.5, marker=r'$\cdot$',label="Phone")
+#plots points of Nodes
+for i in range(len(phoneSet)):
+    graph.scatter(list(phoneSet)[i].xpos, list(phoneSet)[i].ypos, 100, c="black", alpha=0.5, marker=r'$\cdot$',label="Phone")
     
 #test circles
 graph.add_artist(plt.Circle((neighborhood[0].xpos, neighborhood[0].ypos), 100, color = "red", fill = False))
@@ -19,55 +20,9 @@ graph.add_artist(plt.Circle((neighborhood[1].xpos, neighborhood[1].ypos), 100, c
 graph.add_artist(plt.Circle((neighborhood[2].xpos, neighborhood[2].ypos), 100, color = "orange", fill = False))
 graph.add_artist(plt.Circle((neighborhood[3].xpos, neighborhood[3].ypos), 100, color = "blue", fill = False))
 
-#testing greedy
-#temp budget
-budget = 10
-cheapest = 0
-while budget > 0:
-    for i in range(num):
-        if phoneArr[i].visited == False:
-            cheapest = i
-            break
-    print("Budget: ",round(budget, 2))
-    #loop through and find the cheapest Phone that is enabled
-    for i in range(num):
-        if(phoneArr[i].costPerPhone < phoneArr[cheapest].costPerPhone) & (phoneArr[i].visited == False):
-            cheapest = i
-    if (budget - phoneArr[cheapest].cost < 0):
-        break
-    else:
-        budget = budget - phoneArr[cheapest].cost
-        phoneArr[cheapest].visited = True
-<<<<<<< Updated upstream
-        for i in range(num):
-            if phoneArr[cheapest].inRangeArr[i] == True:
-                phoneArr[i].visited = True
-                for i in range(num):
-                    if phoneArr[cheapest].inRangeArr[i] == True:
-                        phoneArr[i].numInRange = phoneArr[i].numInRange - 1
-                        if phoneArr[i].numInRange > 0:
-                            phoneArr[i].costPerNode = round(phoneArr[i].cost / phoneArr[i].numInRange, 2)
-                        else:
-                            phoneArr[i].costPerNode = phoneArr[i].cost
-=======
-        for j in range(num):
-            if phoneArr[cheapest].inRangeArr[j] == True:
-                phoneArr[j].visited = True
-                for m in range(num):
-                    if phoneArr[cheapest].inRangeArr[m] == True:
-                        phoneArr[m].numInRange = phoneArr[m].numInRange - 1
-                        if phoneArr[m].numInRange > 0:
-                            phoneArr[m].costPerPhone = round(phoneArr[m].cost / phoneArr[m].numInRange + 1, 2)
-                        else:
-                            phoneArr[m].costPerPhone = phoneArr[m].cost
->>>>>>> Stashed changes
+for i in range(len(purchaseSet)):
+    graph.add_artist(plt.Circle((list(purchaseSet)[i].xpos, list(purchaseSet)[i].ypos), list(purchaseSet)[i].range, color = "green", fill = False))
 
-        print("Buy Phone:", cheapest, "cost:", phoneArr[cheapest].cost, "costperPhone:", phoneArr[cheapest].costPerPhone)
-
-    #make circle
-        graph.add_artist(plt.Circle((phoneArr[cheapest].xpos, phoneArr[cheapest].ypos), phoneArr[cheapest].range, color = "green", fill = False))
- 
-    #Then take all Phones in range of this one and disable them
 
 #size of graph
 plt.xlim([0,1000])
--- createNodes.py
+++ createNodes.py
@@ -6,20 +6,29 @@ class Coordinates:
         self.xpos = xpos
         self.ypos = ypos
 class Phone(Coordinates):
-    def __init__(self,num, xpos, ypos, cost):
+    def __init__(self,xpos, ypos, cost):
         Coordinates.__init__(self, xpos, ypos)
         self.cost = cost
         self.costPerPhone = 0
         self.range = 50
         self.visited = bool(0)
         self.inRangeSet = set()
-        self.numInRange = 0       
+        self.numInRange = 0     
+    def addPhone(self, phone):
+        self.inRangeSet.add(phone)
+        self.numInRange = len(self.inRangeSet)
+        self.findCPF()
+    def removePhone(self, phone):
+        self.inRangeSet.discard(phone)
+        self.numInRange = len(self.inRangeSet)
+        self.findCPF()
     def findCPF(self):
-        self.costPerPhone = round(self.cost/(self.numInRange + 1), 2)
+        self.costPerPhone = round(self.cost/(self.numInRange), 2)
         
 num = 100
+budget = 5
 
-phoneArr = [Phone] * num
+phoneSet = set()
 neighborhood = [Coordinates] * 4
 
 #makes cluster of Phones in Neighborhoods with different costs
@@ -28,14 +37,15 @@ for i in range(4):
     print("Neighborhood",i)
     randx = random.randint(200, 800)
     randy = random.randint(200, 800)
+    randRange = int(random.uniform(0.2 * num + 0.5, 0.3 * num + 0.5))
     neighborhood[i] = Coordinates(randx, randy)
-    if k < 0.70 * num:
-        for j in range(int(random.uniform(0.2 * num + 0.5, 0.3 * num + 0.5))):
+    if k + randRange < 0.70 * num:
+        for j in range(randRange):
             posX = neighborhood[i].xpos + random.randint(-100,100)
             posY = neighborhood[i].ypos + random.randint(-100,100)
             randCost = round(random.uniform(0.1 + i + i * 0.5, 1 + i * 1.5), 2)
-            phoneArr[k] = Phone(num, posX, posY, randCost)
-            print("Phone:", k, "cost:",phoneArr[k].cost)
+            phoneSet.add(Phone(posX, posY, randCost))
+            print("Phone:", k, "cost:",list(phoneSet)[k].cost)
             k += 1
     else:
         randx = random.randint(200, 800)
@@ -45,33 +55,23 @@ for i in range(4):
             posX = neighborhood[i].xpos + random.randint(-100,100)
             posY = neighborhood[i].ypos + random.randint(-100,100)
             randCost = round(random.uniform(0.1 + i + i * 0.5, 1 + i * 1.5),2)
-            phoneArr[k] = Phone(num, posX, posY, randCost)
-            print("Phone:", k, "cost:",phoneArr[k].cost)
+            phoneSet.add(Phone(posX, posY, randCost))
+            print("Phone:", k, "cost:",list(phoneSet)[k].cost)
             k += 1
 
 #makes set for each Phone of Phones that are within range
+for i in range(len(phoneSet)):
+    for j in range(len(phoneSet)):                
+        distance = math.sqrt(math.pow(list(phoneSet)[i].xpos - list(phoneSet)[j].xpos, 2) + math.pow(list(phoneSet)[i].ypos - list(phoneSet)[j].ypos, 2))
+        if (distance <= list(phoneSet)[i].range):
+            list(phoneSet)[i].addPhone(list(phoneSet)[j])
 for i in range(num):
-    for j in range(num):
-        if (i != j):
-            distance = math.sqrt(math.pow(phoneArr[i].xpos - phoneArr[j].xpos, 2) + math.pow(phoneArr[i].ypos - phoneArr[j].ypos, 2))
-            if (distance <= phoneArr[i].range):
-                phoneArr[i].inRangeSet.add(phoneArr[j])
-                phoneArr[i].numInRange = len(phoneArr[i].inRangeSet)
-                
-for i in range(num):
-    phoneArr[i].findCPF()
+    print("Phone:", i, "numInRange:", list(phoneSet)[i].numInRange, "cost:", list(phoneSet)[i].cost, "costPerPhone:", list(phoneSet)[i].costPerPhone)
 
-for i in range(num):
-    print("Phone:", i, "numInRange:", phoneArr[i].numInRange, "cost:", phoneArr[i].cost, "costPerPhone:", phoneArr[i].costPerPhone)
-
+#algorithm
+tempSet = phoneSet.copy()
 purchaseSet = set()
-budget = 10
-tempSet = set()
-for i in range(num):
-    tempSet.add(phoneArr[i])
-
 cheapest = list(tempSet)[0]
-
 while budget > 0:
     print("Budget:", round(budget, 2))
     cheapest = list(tempSet)[0]
@@ -83,10 +83,11 @@ while budget > 0:
     else:
         budget -= cheapest.cost
         purchaseSet.add(cheapest)
-        for i in range(cheapest.numInRange):
-            tempSet.difference(cheapest.inRangeSet)
+        print("Purchased Phone:", cheapest,"numInRange:", cheapest.numInRange, "cost:", cheapest.cost, "costPerPhone:", cheapest.costPerPhone)
+    tempSet = tempSet.difference(cheapest.inRangeSet)
+    tempSet.discard(cheapest)
 
-print(purchaseSet)
+print("Purchased Set:", purchaseSet)
 
 
 #test print
