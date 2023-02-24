55c3f50ef7087f75b9cf37b90899b30f6408a4ad
Ago0495 <116462335+Ago0495@users.noreply.github.com>
2023-02-23 23:06:51 -0500

    Added new features
    -set seed
    -generate types (cluster or distributed)
    -colored dots based on cost of phone
    -added comments

--- Graph.py
+++ Graph.py
@@ -1,23 +1,29 @@
-from createNodes import Phone
 from createNodes import phoneSet
-from createNodes import neighborhood
+from createNodes import cluster
 from createNodes import purchaseSet
 import matplotlib.pyplot as plt
-import numpy as np
-import random
 
 fig, graph = plt.subplots()
 
 #plots points of Nodes
+#red = (0.1 - 1), orange = (1.1 - 2.5), blue = (2.6 - 4), green = (4.1 - 5.5)
 for i in range(len(phoneSet)):
-    graph.scatter(list(phoneSet)[i].xpos, list(phoneSet)[i].ypos, 100, c="black", alpha=0.5, marker=r'$\cdot$',label="Phone")
-    
+    if list(phoneSet)[i].cost <= 1:
+        graph.scatter(list(phoneSet)[i].xpos, list(phoneSet)[i].ypos, 100, c="red", alpha=0.5, marker=r'$\cdot$',label="Phone")
+    elif list(phoneSet)[i].cost <= 2.5:
+        graph.scatter(list(phoneSet)[i].xpos, list(phoneSet)[i].ypos, 100, c="orange", alpha=0.5, marker=r'$\cdot$',label="Phone")
+    elif list(phoneSet)[i].cost <= 4:
+        graph.scatter(list(phoneSet)[i].xpos, list(phoneSet)[i].ypos, 100, c="blue", alpha=0.5, marker=r'$\cdot$',label="Phone")
+    elif list(phoneSet)[i].cost <= 5.5:
+        graph.scatter(list(phoneSet)[i].xpos, list(phoneSet)[i].ypos, 100, c="green", alpha=0.5, marker=r'$\cdot$',label="Phone")
+ 
 #test circles
-graph.add_artist(plt.Circle((neighborhood[0].xpos, neighborhood[0].ypos), 100, color = "red", fill = False))
-graph.add_artist(plt.Circle((neighborhood[1].xpos, neighborhood[1].ypos), 100, color = "yellow", fill = False))
-graph.add_artist(plt.Circle((neighborhood[2].xpos, neighborhood[2].ypos), 100, color = "orange", fill = False))
-graph.add_artist(plt.Circle((neighborhood[3].xpos, neighborhood[3].ypos), 100, color = "blue", fill = False))
+# graph.add_artist(plt.Circle((cluster[0].xpos, cluster[0].ypos), 100, color = "red", fill = False))
+# graph.add_artist(plt.Circle((cluster[1].xpos, cluster[1].ypos), 100, color = "yellow", fill = False))
+# graph.add_artist(plt.Circle((cluster[2].xpos, cluster[2].ypos), 100, color = "orange", fill = False))
+# graph.add_artist(plt.Circle((cluster[3].xpos, cluster[3].ypos), 100, color = "blue", fill = False))
 
+#draws circles around the purchased phones
 for i in range(len(purchaseSet)):
     graph.add_artist(plt.Circle((list(purchaseSet)[i].xpos, list(purchaseSet)[i].ypos), list(purchaseSet)[i].range, color = "green", fill = False))
 
--- createNodes.py
+++ createNodes.py
@@ -1,5 +1,6 @@
 import random
 import math 
+import numpy as np
         
 class Coordinates:
     def __init__(self, xpos, ypos):
@@ -24,25 +25,49 @@ class Phone(Coordinates):
         self.findCPF()
     def findCPF(self):
         self.costPerPhone = round(self.cost/(self.numInRange), 2)
-        
-num = 10000
+
+np.random.seed(0)
+num = 100
 budget = 50
+#0 = cluster, 1 = distributed
+generateType = 0
 
 phoneSet = set()
-neighborhood = [Coordinates] * 4
+cluster = [Coordinates] * 4
 
-#makes cluster of Phones in Neighborhoods with different costs
+match generateType:
+    #Cluster
+    case 0:
+        rangeLower = -100
+        rangeHigher = 100
+        clusterLower = 200
+        clusterHigher = 800
+    #Randomly Distributed
+    case 1:
+        rangeLower = -500
+        rangeHigher = 500
+        clusterLower = 499
+        clusterHigher = 500
+    #Defaults to cluster
+    case _:
+        rangeLower = -100
+        rangeHigher = 100
+        clusterLower = 200
+        clusterHigher = 800        
+        
+#generates phones with different costs and connects them as they generate
+#generates first 3 waves costing (0.1 - 1), (1.1 - 2.5), (2.6 - 4) then remaining wave costing (4.1 - 5.5) 
 for i in range(4):
-    print("Neighborhood",i)
-    randx = random.randint(200, 800)
-    randy = random.randint(200, 800)
-    randRange = int(random.uniform(0.2 * num + 0.5, 0.3 * num + 0.5))
-    neighborhood[i] = Coordinates(randx, randy)
+    print("Cluster",i)
+    randx = np.random.randint(clusterLower, clusterHigher)
+    randy = np.random.randint(clusterLower, clusterHigher)
+    randRange = int(np.random.uniform(0.2 * num + 0.5, 0.3 * num + 0.5))
+    cluster[i] = Coordinates(randx, randy)
     if len(phoneSet) < 0.70 * num:
         for j in range(randRange):
-            posX = neighborhood[i].xpos + random.randint(-100,100)
-            posY = neighborhood[i].ypos + random.randint(-100,100)
-            randCost = round(random.uniform(0.1 + i + i * 0.5, 1 + i * 1.5), 2)
+            posX = cluster[i].xpos + np.random.randint(rangeLower,rangeHigher)
+            posY = cluster[i].ypos + np.random.randint(rangeLower,rangeHigher)
+            randCost = round(np.random.uniform(0.1 + i + i * 0.5, 1 + i * 1.5), 2)
             newPhone = Phone(posX, posY, randCost)
             phoneSet.add(newPhone)
             for k in range(len(phoneSet)):            
@@ -53,13 +78,13 @@ for i in range(4):
                     list(phoneSet)[k].addPhone(newPhone)
             print("Generated Phone:", len(phoneSet) -1, "xpos:", newPhone.xpos, "ypos:", newPhone.ypos, "cost:", newPhone.cost)
     else:
-        randx = random.randint(200, 800)
-        randy = random.randint(200, 800)
-        neighborhood[i] = Coordinates(randx, randy)
+        randx = np.random.randint(clusterLower,clusterHigher)
+        randy = np.random.randint(clusterLower,clusterHigher)
+        cluster[i] = Coordinates(randx, randy)
         for j in range(num - len(phoneSet)):
-            posX = neighborhood[i].xpos + random.randint(-100,100)
-            posY = neighborhood[i].ypos + random.randint(-100,100)
-            randCost = round(random.uniform(0.1 + i + i * 0.5, 1 + i * 1.5),2)
+            posX = cluster[i].xpos + np.random.randint(rangeLower,rangeHigher)
+            posY = cluster[i].ypos + np.random.randint(rangeLower,rangeHigher)
+            randCost = round(np.random.uniform(0.1 + i + i * 0.5, 1 + i * 1.5),2)
             newPhone = Phone(posX, posY, randCost)  
             phoneSet.add(newPhone)          
             for k in range(len(phoneSet)):
@@ -70,24 +95,24 @@ for i in range(4):
                     list(phoneSet)[k].addPhone(newPhone)
             print("Generated Phone:", len(phoneSet) -1, "xpos:", newPhone.xpos, "ypos:", newPhone.ypos, "cost:", newPhone.cost)
 
-#makes set for each Phone of Phones that are within range
-# for i in range(len(phoneSet)):
-#     for j in range(len(phoneSet)):                
-#         #print("Phone:",i,"Pinging",j)
-#         distance = math.sqrt(math.pow(list(phoneSet)[i].xpos - list(phoneSet)[j].xpos, 2) + math.pow(list(phoneSet)[i].ypos - list(phoneSet)[j].ypos, 2))
-#         if (distance <= list(phoneSet)[i].range):
-#             list(phoneSet)[i].addPhone(list(phoneSet)[j])
 for i in range(len(phoneSet)):
     print("Phone:", i, "numInRange:", list(phoneSet)[i].numInRange, "cost:", list(phoneSet)[i].cost, "costPerPhone:", list(phoneSet)[i].costPerPhone)
 
 #algorithm
+#makes a copy of phoneSet called tempSet
+#while the budget is greater than 0,
+#it first checks if tempSet is empty, if not then continue
+#skims through tempSet for phone with the lowest cost per phone 
+#checks if the cost of the phone will break the budget, if not then continue
+#subtract cost from budget and add phone to purchasedSet
+#descards any phones within range of the purchased phone form the tempSet
+#repeat until out of phones or budget cannot afford anymore phones
 tempSet = phoneSet.copy()
 purchaseSet = set()
-cheapest = list(tempSet)[0]
 while budget > 0:
     print("Budget:", round(budget, 2),"tempSet len:", len(tempSet))
     if len(tempSet) == 0:
-        print("len break")
+        print("length break")
         break
     cheapest = list(tempSet)[0]
     for i in range(len(tempSet)):
@@ -95,20 +120,12 @@ while budget > 0:
             cheapest = list(tempSet)[i]
     if budget - cheapest.cost < 0:
         print("attempt purchase:", cheapest.cost)
-        print("budget break")
+        print("bidget break")
         break
     else:
         budget -= cheapest.cost
         purchaseSet.add(cheapest)
         print("Purchased Phone:", cheapest,"numInRange:", cheapest.numInRange, "cost:", cheapest.cost, "costPerPhone:", cheapest.costPerPhone)
     tempSet = tempSet.difference(cheapest.inRangeSet)
-    tempSet = tempSet.difference(purchaseSet)
 
-print("Purchased Set:", purchaseSet)
-
-
-#test print
-#print("Phone",num - 1,  "x:", phoneArr[num - 1].xpos, "y:", phoneArr[num - 1].ypos, "numInRange:", phoneArr[num - 1].numInRange, "cost:", phoneArr[num - 1].cost, "costperPhone:", phoneArr[num - 1].costPerPhone)
-
-
-        
+print("Purchased Set:", purchaseSet)       
