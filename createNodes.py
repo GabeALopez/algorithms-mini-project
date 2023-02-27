1e97b081b902d9823a4719f9d750807becaa9303
Gabriel Lopez <gabriel.andres.lopez03@gmail.com>
2023-02-27 14:46:02 -0500

    Minor changes

--- createNodes.py
+++ createNodes.py
@@ -28,7 +28,7 @@ class Phone(Coordinates):
 
 #np.random.seed(0)
 num = 100
-budget = 50
+budget = 5
 #0 = cluster, 1 = distributed
 generateType = 0
 algorithmType = 2
@@ -144,9 +144,12 @@ match algorithmType:
             randPhone = list(tempSet)[rn-1];
 
             if budget > randPhone.cost: 
-                budget - randPhone.cost
+                budget -= randPhone.cost
+                purchaseSet.add(randPhone)
+
             elif budget < randPhone.cost:
                 continue
+
         
         #print out the nodes to graph 
 
